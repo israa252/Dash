6938f84e37b55503a687796bb6010f7035537c4b
israa252 <99605048+israa252@users.noreply.github.com>
2023-08-19 18:49:13 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Layout from "/components/Layout";
+ 
+
+export default function Home() {
+   
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Hello 
+      </h2>
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+  
+       
+      </div>
+    </div>
+  </Layout>
+}
